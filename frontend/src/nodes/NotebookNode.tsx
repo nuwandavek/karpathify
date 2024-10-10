0c67a0d19f79f65e4d8d5fcff79c562b4df4cade
nuwandavek <vivekaithal44@gmail.com>
2024-10-10 00:05:33 -0700

    add latex support

--- frontend/src/nodes/NotebookNode.tsx
+++ frontend/src/nodes/NotebookNode.tsx
@@ -6,9 +6,13 @@ import vsd from 'react-syntax-highlighter/dist/esm/styles/prism/vsc-dark-plus';
 import Markdown from 'react-markdown'
 import { useState } from 'react';
 import { Lesson, CodeBlock } from './types';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import 'katex/dist/katex.min.css';
+
 SyntaxHighlighter.registerLanguage('python', python);
 
-import dataFile from '../../../backend/examples/example5.json'
+import dataFile from '../../../backend/examples/example10.json'
 
 const lessons: Lesson[] = dataFile.lessons
 
@@ -18,16 +22,20 @@ const Block = ({ code, notes }: CodeBlock) => {
     <div style={{display: "flex", flexDirection: "row", justifyContent:"stretch",
       alignItems:"stretch", background: isHovered ? "#333" : "", padding: "10px"}}
     onMouseOver={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>
-      <div style={{width: "50%", overflow: "scroll", border: "1px solid #eee", borderRadius: "5px",
-    margin: "5px"}}>
+      <div style={{width: "50%", overflow: "scroll", border: isHovered ? "1px solid #f1c40f" : "1px solid #eee", borderRadius: "5px",
+    margin: "5px", background: "#1e1e1e"}}>
         <SyntaxHighlighter language={"python"} style={vsd}>
           {code}
         </SyntaxHighlighter>
       </div>
       <div style={{width: "50%", display: "flex", flexDirection: "column",
         justifyContent:"start", alignItems:"start", textAlign: "left", padding: "10px",
-        border: "1px solid #eee", borderRadius: "5px", margin: "5px"}}>
-        <Markdown>{notes}</Markdown>
+        border: isHovered ? "1px solid #f1c40f" : "1px solid #eee", borderRadius: "5px", margin: "5px"}}>
+        <Markdown
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[rehypeKatex]}>
+          {notes}
+        </Markdown>
       </div>
     </div>
   )
@@ -35,7 +43,7 @@ const Block = ({ code, notes }: CodeBlock) => {
 
 export function NotebookNode({data}: NodeProps<NotebookNode>) {
   return (
-    <div className="react-flow__node-default" style={{
+    <div className="react-flow__node-default nowheel" style={{
       width: '100%',
       maxWidth: "1000px",
       display: 'flex',
